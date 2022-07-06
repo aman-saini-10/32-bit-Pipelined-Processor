a99e106048127aca6f9dcc24bf9bea8d15e4a676
Aman Saini <2020eeb1155@iitrpr.ac.in>
2022-07-06 22:37:35 +0530

    Removed no ops, after adding Data Forwarding
    Removed the no operation instructions, after adding the data forwarding Paths in the Processor

--- RISC/testbench.v
+++ RISC/testbench.v
@@ -21,70 +21,20 @@ module test;
                           P1.r[0], P1.r[1], P1.r[2],  P1.r[3],  P1.r[4],  P1.r[5],  P1.r[6],  P1.r[7], 
                           P1.r[8], P1.r[9], P1.r[10], P1.r[11], P1.r[12], P1.r[13], P1.r[14], P1.r[15]);
 
-                          
-        # 50 reset <= 0;
+        //$monitor("time = ", $time, "\n %h\n %h\n %h\n %h\n %h\n %h\n %h", P1.IF_OF, P1.OF_EX, P1.EX_MA, P1.MA_RW, P1.A, P1.B, P1.op2);        
+        # 7 reset <= 0;
     end
     // LET'S STORE THE PROGRAM INTO THE MEMORY FIRST THEN, 
     // WE CAN PROCEED WITH THE EXECUTION IN THE PROCESSOR
     // FOR THIS TESTBENCH WE WILL SIMULATE A SIMPLERISC CODE TO COMPUTE 31*29 - 50
 
     initial begin
-        P1.write    <= 0; // Set the Write Signal to 0
-        P1.addr2    <= 0; // Set the address of the first instruction as 0
-        P1.data     <= 32'h4C40001F; #1; // Mov r1, 31
-        P1.write    <= 1; #1; 
+        P1.Instruction_MEM[0] <= 32'h4C40001F; // Mov r1, 31
+        P1.Instruction_MEM[1] <= 32'h4C80001D; // Mov r2, 29
+        P1.Instruction_MEM[2] <= 32'h10C48000; // Mul r3, r1, r2
+        P1.Instruction_MEM[3] <= 32'h0D0C0032; // Sub r4, r3, 50
 
-        P1.addr2    <= 1;
-        P1.data     <= 32'h4C80001D;    // Mov r2, 29
-        P1.write    <= 0; #1;
-        P1.write    <= 1; #1; 
-
-
-        // PROCESSOR NEEDS TO BE STALLED FOR 3 CYCLES TO PREVENT RAW DEPENDENCY
-
-        P1.addr2    <= 2; 
-        P1.data     <= 32'h68000000; //no operation
-        P1.write    <= 0; #1;
-        P1.write    <= 1; #1; 
-                
-        P1.addr2    <= 3; 
-        P1.data     <= 32'h68000000; //no operation
-        P1.write    <= 0; #1;
-        P1.write    <= 1; #1; 
-
-        P1.addr2    <= 4; 
-        P1.data     <= 32'h68000000; //no operation
-        P1.write    <= 0; #1;
-        P1.write    <= 1; #1; 
-
-        P1.addr2    <= 5; 
-        P1.data     <= 32'h10C48000; // Mul r3, r1, r2
-        P1.write    <= 0; #1;
-        P1.write    <= 1; #1; 
-
-        // STALL BEGIN
-
-        P1.addr2    <= 6; 
-        P1.data     <= 32'h68000000; //no operation
-        P1.write    <= 0; #1;
-        P1.write    <= 1; #1; 
-
-        P1.addr2    <= 7; 
-        P1.data     <= 32'h68000000; //no operation
-        P1.write    <= 0; #1;
-        P1.write    <= 1; #1; 
-
-        P1.addr2    <= 8; 
-        P1.data     <= 32'h68000000; //no operation
-        P1.write    <= 0; #1;
-        P1.write    <= 1; #1; 
-
-        P1.addr2    <= 9;
-        P1.data     <= 32'h0D0C0032; // Sub r4, r3, 50
-        P1.write    <= 0; #1;
-        P1.write    <= 1;
+        #200 $finish;
     end
-
-    
 
 endmodule
